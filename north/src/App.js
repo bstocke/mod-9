31c7f3745ed467c5cb13de422bd0186a1fcb314d
bstocke <dueling3500@gmail.com>
2022-03-22 04:48:52 -0400

    commit on 'main'

--- north/src/App.js
+++ north/src/App.js
@@ -1,3 +1,31 @@
+import React, {useState} from 'react'
+import './index.css'
 
+const App = () => {
+  const [count, setCount] = useState(0)
+  const [dark, setDark] = useState(false)
+  //create working constructors
+  const decrement = () => {
+    setCount(count -1)
+  }
+
+  const increment = () => {
+    setCount(count +1)
+  }
+
+  const toggleDark = () => {
+    setDark(!dark)
+  }
+
+return(
+  <div className= 'App'>
+    <button onClick={decrement}>Minus 1</button>
+    <button onClick={increment}>Add 1</button>
+    <button onClick={toggleDark}>Toggle Dark</button>
+    <br/>
+  </div>
+
+  )
+}
 
 export default App;
